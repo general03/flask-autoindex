b052bfa5906608f56172cfd15316dcca9978a45c
Heungsub Lee <h@subl.ee>
2011-07-06 00:16:49 +0900

    version up

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ def run_tests():
 
 setup(
     name='Flask-AutoIndex',
-    version='0.3.2',
+    version='0.4.0',
     url='http://github.com/sublee/flask-autoindex',
     license='BSD',
     author='Heungsub Lee',
